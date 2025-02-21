73e28acea0ede6913fd3ee18001fb51289165c60
Zacharias Anastasakis <Zacharias.Anastasakis@gr.ey.com>
2025-02-21 14:18:32 +0200

    minor changes

--- chat_ui/utils/prompts.py
+++ chat_ui/utils/prompts.py
@@ -1,26 +1,3 @@
-####################################################### Reranking #######################################################
-rerank_system_prompt = '''You are Rank-GPT, an intelligent assistant that can rank documents based on their relevancy to the query.
-Given a query and some documents, each indicated by number identifier, your task is provide a score from 0 to 10 which measures how relevant you think the documents are to the question.
-Follow all the steps mentioned below:
-
-Steps:
-1. Iterate through each document and identify if it is relevant to the query.
-2. Assign a score from 0 to 10 based on how relevant is the document to the query, where 0 indicates that the document is completely irrelevant and 10 the document is highly relevant.
-3. If a document provides only general information about the topic, such as cover pages, tables of contents, or introductory sections it is completely irrelevant.
-'''
-
-rerank_user_prompt = """
-Query:
-{query}
-
-Documents:
-{documents}
-
-Think this through step by step.
-
-At the end of your response you should provide a list of scores contained in <result> </result> tags, where the index of each score is the number of the document. For example the list <result>[5, 1, 10]</result> means that 3 documents were provided with scores 5, 1 and 10 respectively.
-"""
-
 ####################################################### Final Generation #######################################################
 
 generation_system_prompt = """You are a helpful assistant that answers questions based on the provided context. You will be given a query and some relevant documents and your task is to answer the query based solely on the provided contenxt-sources.
@@ -42,6 +19,3 @@ Question: {question}
 
 Include also references to the sources in your response in format (page <page-num>, <filename>).
 """
-####################################################### Intent Classification #######################################################
-
-# put your '#' separated prompt here for intent classification
